b79826fb1a302ddcee85631356f780a669ac5347
Vitalii Protsenko <vp412@ic.ac.uk>
2015-10-31 18:27:41 +0000

    last test

--- tests/correct/nestedmulexprcomplex.c
+++ tests/correct/nestedmulexprcomplex.c
@@ -2,12 +2,10 @@ int x;
 int y;
 
 int f()
-    requires x != 0 && y != 0
-    ensures \result == !\old(x)
+    requires x != 0 && y != 0,
+    ensures \result == \old(x)
 {
-    x = y % 4 << 12 * 51
-            ? !x / !y
-            : (y / x / (y * (x << 2)));
-
-    return x;
+    return y % 4 << 12 * 51
+               ? (y / x / (y * (x << 2)))
+               : x / !y;
 }
